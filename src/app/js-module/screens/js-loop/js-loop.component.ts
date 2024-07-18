f00aa7039eeeed07a71030a3db7f4a0fccd2369e
Sids-projects <siddarthg.dev@gmail.com>
2024-07-18 17:29:26 +0530

    Added array and objects

--- src/app/js-module/screens/js-loop/js-loop.component.ts
+++ src/app/js-module/screens/js-loop/js-loop.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SharedService } from 'src/app/shared.service';
 
 @Component({
   selector: 'app-js-loop',
   templateUrl: './js-loop.component.html',
-  styleUrls: ['./js-loop.component.scss']
+  styleUrls: ['./js-loop.component.scss'],
 })
 export class JsLoopComponent {
+  langEng!: boolean;
+  private subscription!: Subscription;
 
+  constructor(private sharedService: SharedService) {}
+
+  ngOnInit() {
+    this.subscription = this.sharedService.langEng$.subscribe((value) => {
+      this.langEng = value;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
